7bb9d846b1d478a6bdff3f4fc1a4c603f8f7fc56
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-09-18 21:17:45 +0200

    Update phoneAndEmail.py
    First version of phoneRegex

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -4,6 +4,20 @@ import re, pyperclip
 
 #TODO: Create a regex object for phone numbers
 
+phoneRegex = re.compile(r'''
+
+( 0\d | \+\d\d\s?\d | 00\s?\d\d\s?\d)			#first 2 digits -or- international area code with plus and first digit -or- intenational area code with space and first digit
+(\s | \.)?										#space separator
+(\d\d)											#second 2 digits
+(\s | \.)?										#space separator
+(\d\d)											#third 2 digits
+(\s | \.)?										#space separator
+(\d\d)											#fourth 2 digits
+(\s | \.)?										#space separator
+(\d\d)											#last 2 digits
+
+	''', re.VERBOSE)
+
 #TODO: Create a regex for email adress
 
 #TODO: Get the text off the clipboard
