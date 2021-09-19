8d0a857398cd70eab854304c285034c51efe8fa0
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-09-19 13:19:24 +0200

    Update phoneAndEmail.py

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -5,7 +5,7 @@ import re, pyperclip
 # Create a regex object for phone numbers
 
 phoneRegex = re.compile(r'''
-
+(
 ( 0\d | \+\d\d\s?\d | 00\s?\d\d\s?\d)			#first 2 digits -or- international area code with plus and first digit -or- intenational area code with space and first digit
 (\s | \.)?										#space or dot separator
 (\d\d)											#second 2 digits
@@ -15,7 +15,7 @@ phoneRegex = re.compile(r'''
 (\d\d)											#fourth 2 digits
 (\s | \.)?										#space or dot separator
 (\d\d)											#last 2 digits
-
+)
 	''', re.VERBOSE)
 
 # Create a regex for email adress
@@ -29,8 +29,18 @@ emailRegex = re.compile(r'''
 
 	''', re.VERBOSE)
 
-#TODO: Get the text off the clipboard
+# Get the text off the clipboard
 
-#TODO: Extract the email/phone from this text
+text = pyperclip.paste()
+
+# Extract the email/phone from this text
+
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+
+for phoneNumber in extractedPhone:
+	allPhoneNumbers.append(phoneNumber[0])
 
 #TODO: Copy the extracted email/phone to the clipboard
